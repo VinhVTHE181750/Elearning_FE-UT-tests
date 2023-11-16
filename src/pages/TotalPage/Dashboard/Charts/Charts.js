9eebeb05348769fe624bafb956ae32251f3ee107
minhnhhe161877 <minhnhhe161877@fpt.edu.vn>
2023-11-16 20:47:49 +0700

    Fix Barchart

--- src/pages/TotalPage/Dashboard/Charts/Charts.js
+++ src/pages/TotalPage/Dashboard/Charts/Charts.js
@@ -1,23 +1,27 @@
-import { BarChart } from '@mui/icons-material';
+import { BarChart } from '@mui/x-charts';
 import React from 'react';
 
 export default function Charts({ list }) {
+  const modifiedList = list.map((value) => value / 2);
   return (
-    <BarChart
-      xAxis={[
-        {
-          id: 'barCategories',
-          data: ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          scaleType: 'band',
-        },
-      ]}
-      series={[
-        {
-          data: list,
-        },
-      ]}
-      width={500}
-      height={300}
-    />
+    <div className="chart-container">
+      <BarChart
+        xAxis={[
+          {
+            id: 'barCategories',
+            data: ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            scaleType: 'band',
+          },
+        ]}
+        series={[
+          {
+            data: modifiedList,
+          },
+        ]}
+        width={700}
+        height={500}
+        margin={{ right: 110, bottom: 50, left: 100 }}
+      />
+    </div>
   );
 }
