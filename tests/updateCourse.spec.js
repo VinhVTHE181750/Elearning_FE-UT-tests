86eddd12686716b342236f76e0d5e98d77d9a7c6
HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>
2024-07-19 16:08:24 +0700

    Update updateQuestion test

--- tests/updateCourse.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/edit-question/1');
-  await page.getByLabel('Question Name:').click();
-  await page.getByLabel('Question Name:').fill('name');
-  await page.getByLabel('Question Type:Select Question').selectOption('ONE_CHOICE');
-  await page.getByRole('button', { name: 'Edit' }).click();
-});
---
+++ tests/updateQuestion.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/edit-question/1');
+  await page.getByLabel('Question Name:').click();
+  await page.getByLabel('Question Name:').fill('name');
+  await page.getByLabel('Question Type:Select Question').selectOption('ONE_CHOICE');
+  await page.getByRole('button', { name: 'Edit' }).click();
+});
